3ab4725f89105caae2cfe2754b2e12e64628a488
anniehedgpeth <annie@Annies-MBP.tx.rr.com>
2017-03-02 11:10:13 -0600

    methods refactored

--- writing-methods.rb
+++ writing-methods.rb
@@ -98,6 +98,41 @@
 
 puts '***********************************************'
 
+def ask(question)
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if reply == 'yes'
+      return true
+    elsif reply == 'no'
+      return false
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
+end
+
+puts 'Hello, and thank you for...'
+puts
+
+ask 'Do you like eating tacos?'
+ask 'Do you like eating burritos?'
+wets_bed = ask 'Do you wet the bed?'
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for...'
+puts
+puts wets_bed
+
+puts '***********************************************'
+
 def say_moo
   puts 'moooooo...'
 end
@@ -160,3 +195,43 @@ puts '***********************************************'
 
 return_value = puts 'This returned: '
 puts return_value
+
+def say_moo number_of_moos
+  puts 'mooooooo...' * number_of_moos
+  'yellow submarine'
+end
+
+x = say_moo(3)
+puts "#{x}, dude..."
+puts "#{x}, dude..."
+
+puts '***********************************************'
+
+def favorite_food(name)
+  if name == 'Lister'
+    return 'vindaloo'
+  end
+
+  if name == 'Rimmer'
+    return 'mashed potatoes'
+  end
+
+  'hard to say...maybe fried plantain?'
+end
+
+def favorite_drink name
+  if name == 'Jean-Luc'
+    'tea, Earl Grey, hot'
+  elsif name == 'Kathryn'
+    'coffee, black'
+  else
+    'perhaps...horchata?'
+  end
+end
+
+puts favorite_food('Rimmer')
+puts favorite_food('Lister')
+puts favorite_food('Cher')
+puts favorite_drink('Kathryn')
+puts favorite_drink('Oprah')
+puts favorite_drink('Jean-Luc')
